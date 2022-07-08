6607cc7679f20dc6c5ef14093da4a17672ae4ff4
YusukeNakatsubo <nakatsubo.works@gmail.com>
2022-07-08 12:58:26 +0900

    トップページの仮組みが完了

--- pages/index.vue
+++ pages/index.vue
@@ -1,29 +1,31 @@
 <template>
   <div class="index-wrapper">
-    <section class="first panel blue" id="one">
+
+    <section class="index-wrapper__section isFirst panel blue" id="one">
       <h1>Fading Transitions</h1>
       <div class="scroll-down">Scroll up or down<div class="arrow"></div></div>
     </section>
-    <section class="panel red" id="two">
+    <section class="index-wrapper__section panel red" id="two">
       <h1>section 2 red</h1>
     </section>
-    <section class="panel orange" id="three">
+    <section class="index-wrapper__section panel orange" id="three">
       <h1>section 3 orange</h1>
     </section>
-    <section class="panel purple" id="four">
+    <section class="index-wrapper__section panel purple" id="four">
       <h1>section 4 purple</h1>
     </section>
-    <section class="panel green" id="five">
+    <section class="index-wrapper__section panel green" id="five">
       <h1>section 5 green</h1>
     </section>
     
-    <nav>
-      <div><a href="#one" class="js-scroll">Section one</a></div>
-      <div><a href="#two" class="js-scroll">Section two</a></div>
-      <div><a href="#three" class="js-scroll">Section three</a></div>
-      <div><a href="#four" class="js-scroll">Section four</a></div>
-      <div><a href="#five" class="js-scroll">Section five</a></div>
-    </nav>
+    <div class="index-wrapper__list">
+      <div class="index-wrapper__list--item">Section one</div>
+      <div class="index-wrapper__list--item">Section two</div>
+      <div class="index-wrapper__list--item">Section three</div>
+      <div class="index-wrapper__list--item">Section four</div>
+      <div class="index-wrapper__list--item">Section five</div>
+    </div>
+
   </div>  
 </template>
 
@@ -39,16 +41,23 @@ if (process.client) {
 export default {
   name: 'IndexPage',
   mounted() {
+    // Adobe Fonts 
     this.$adobeFonts(document)
 
-    const sections = gsap.utils.toArray("section")
+    // Section transition
+    const sections = gsap.utils.toArray('.index-wrapper__section')
+    const lists = gsap.utils.toArray('.index-wrapper__list--item')
+
     let currentSection = sections[0]
 
-    gsap.defaults({overwrite: 'auto', duration: 1})
+    gsap.defaults({
+      overwrite: 'auto',
+      duration: 1
+    })
 
-    const links = gsap.utils.toArray("nav a")
-
-    gsap.set("body", {height: (sections.length * 100) + "%"})
+    gsap.set('body', {
+      height: (sections.length * 100) + '%'
+    })
 
     sections.forEach((section, i) => {
       ScrollTrigger.create({
@@ -56,90 +65,99 @@ export default {
         end: () => (i + 0.5) * innerHeight,
         scrub: true,
         markers: true,
-        toggleActions: "play reset play reset",
+        toggleActions: 'play reset play reset',
         onToggle: self => self.isActive && setSection(section),
         onEnter: () => {
-          links.forEach((e) => {
-            e.classList.remove("active")
+          lists.forEach((e) => {
+            e.classList.remove('isActive')
           })
-          links[i].classList.add("active")
+          lists[i].classList.add('isActive')
         },
         onEnterBack: () => {
-          links.forEach((e) => {
-            e.classList.remove("active")
+          lists.forEach((e) => {
+            e.classList.remove('isActive')
           })
-          links[i].classList.add("active")
+          lists[i].classList.add('isActive')
         }
       })
     })
 
     function setSection(newSection) {
       if (newSection !== currentSection) {
-       gsap.timeline()
-         .to(currentSection.querySelector("h1"), {y:-30, autoAlpha:0, duration:0.3})
-         .to(currentSection, {autoAlpha:0, duration:0.5})
+        gsap.timeline()
+          .to(currentSection.querySelector('h1'), {
+            y: -30,
+            autoAlpha: 0,
+            duration: 0.3
+          })
+          .to(currentSection, {
+            autoAlpha: 0,
+            duration: 0.5
+          })
 
         gsap.timeline()
-          .to(newSection, {autoAlpha:1, duration:0.5})
-          .fromTo(newSection.querySelector("h1"), {y:30, autoAlpha:0}, {autoAlpha: 1, y: 0, duration:.3})
-
+          .to(newSection, {
+            autoAlpha: 1,
+            duration: 0.5
+          })
+          .fromTo(newSection.querySelector('h1'), {
+            y: 30,
+            autoAlpha: 0
+          }, {
+            autoAlpha: 1,
+            y: 0, duration:.3
+          })
         currentSection = newSection    
       }
     }
-
   },
 }
 </script>
 
-<style>
+<style lang="scss" scoped>
 /* stylelint-disable */
-html, body {
-  height: 100%;
-}
-body { 
-  background-color: #000;
-  font-size: 18px;
-  font-weight: 300;
-}
-.panel {
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-}
-/* .panel h1 {
-  font-size: 1.8em;
-  color: black;
-  font-weight: 300;
-  margin: 0 auto;
-  z-index:5;
-}
-section:not(.first) {
-  opacity: 0;
-  visibility: hidden;
-}  */
+.index-wrapper {
 
-nav {
-  position: fixed;
-  top: 10px;
-  right: 10px;
-  background: black;
-  padding: 0px 10px;
-}
-nav a {
-  color: white;
-  font-size: 1.3em;
-  line-height: 1.7;
-  text-decoration: none;
-}
-nav a:hover {
-  text-decoration: underline;
-}
-nav a.active {
-  color: red;
+  // section
+  &__section {
+    position: fixed;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+
+    &:not(.isFirst) {
+      opacity: 0;
+      visibility: hidden;
+    }
+  }
+
+  // list
+  &__list {
+    position: fixed;
+    top: 10px;
+    right: 10px;
+    background: black;
+    padding: 0px 10px;
+
+    &--item {
+      color: white;
+      font-size: 1.3em;
+      line-height: 1.7;
+      text-decoration: none;
+
+      &:hover {
+        text-decoration: underline;
+      }
+
+      &.isActive {
+        color: red;
+      }
+    }
+  }
 }
 
+/* dummy colors */
 .blue {
   background-color: blue;
 }
